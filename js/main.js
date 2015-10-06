503370f9b3b022a2cd3b0512ac9ba6f39d4a5aab
sara clayton <clay0037@algonquinlive.com>
2015-10-05 20:12:34 -0400

    created a stylized tab window to display content for html section

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var $hamBtn = $('.ham-btn');
+var $panel = $('.panel');
+var $tabs = $('.tabs');
+var $panels = $('.tab-panel');
+
+$hamBtn.on('click', function () {
+  $panel.toggleClass('js-panel-open');
+});
+
+$tabs.on('click', 'a', function (e) {
+  e.preventDefault();
+  var id = $(this).attr('href');
+
+  $panels.filter('.js-appear').removeClass('js-appear');
+  $(id).addClass('js-appear');
+
+  $tabs.find('.js-current').removeClass('js-current');
+  $(this).addClass('js-current');
+});
+
+$('.top-nav').localScroll();
+
+$('.top-scroll').localScroll();
+
+$('.top-scroll-button').localScroll();
+
+$styled.waypoint (function () {
+  $styled.toggleClass('js-active');
+}, {offset: '50%'});
